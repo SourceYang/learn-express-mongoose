fa4af7ba69e3a008d0b9f0010eb4663c990983cd
Yuanyuan Yang <1005842457@qq.com>
2024-03-15 18:09:03 -0400

    Update book.js

--- models/book.js
+++ models/book.js
@@ -3,8 +3,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-  {}
+  {
+    title: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'Author', required: true },
+    summary: { type: String, required: true },
+    ISBN: { type: String, required: true },
+    genre: [{ type: Schema.Types.ObjectId, ref: 'Genre' }],
+    url: { type: String, required: true }
+  }
 );
+
+BookSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/book/' + this._id;
+});
 
 //Export model
 module.exports = mongoose.model('Book', BookSchema);
