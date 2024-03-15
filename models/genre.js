91918c9e7aa7d2c3093e5975f44eaa454df0cac8
Yuanyuan Yang <1005842457@qq.com>
2024-03-15 18:09:25 -0400

    Update genre.js

--- models/genre.js
+++ models/genre.js
@@ -3,9 +3,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var GenreSchema = new Schema(
-  {}
+  {
+    name: { type: String, required: true, minlength: 3, maxlength: 100 },
+    url: { type: String, required: true }
+  }
 );
 
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
 
 //Export model
 module.exports = mongoose.model('Genre', GenreSchema);
