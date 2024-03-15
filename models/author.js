f8dc7156d4d9d53bbfa9576786eaca5b3649535d
Yuanyuan Yang <1005842457@qq.com>
2024-03-15 18:11:46 -0400

    Update author.js

--- models/author.js
+++ models/author.js
@@ -4,10 +4,10 @@ var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
   {
-    first_name: {type: String, required: true, maxLength: 100},
-    family_name: {type: String, required: true, maxLength: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
+    first_name: { type: String, required: true, maxLength: 100 },
+    family_name: { type: String, required: true, maxLength: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
   }
 );
 
@@ -15,20 +15,21 @@ var AuthorSchema = new Schema(
 AuthorSchema
 .virtual('name')
 .get(function () {
-// To avoid errors in cases where an author does not have either a family name or first name
-// We want to make sure we handle the exception by returning an empty string for that case
   var fullname = '';
   if (this.first_name && this.family_name) {
-    fullname = this.family_name + ', ' + this.first_name
-  }
-  if (!this.first_name || !this.family_name) {
-    fullname = '';
+    fullname = this.family_name + ', ' + this.first_name;
   }
   return fullname;
 });
 
 // Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {});
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  var birth = this.date_of_birth ? this.date_of_birth.getFullYear().toString() : '';
+  var death = this.date_of_death ? this.date_of_death.getFullYear().toString() : '';
+  return birth + ' – ' + (death ? death : '');
+});
 
 //Export model
 module.exports = mongoose.model('Author', AuthorSchema);
